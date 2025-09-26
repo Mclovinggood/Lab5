fee1b092c7db9209a47a79449b38543911a36bb2
Mclovinggood <jthand293@gmail.com>
2025-09-26 18:24:40 -0400

    Fetch_games function just gets the games and puts them in a JSON file

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import requests
 
+def fetch_games():
+    url = "https://www.freetogame.com/api/games"
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()
+
 def main():
     print("FreeToGame App Jumpscare!!!")
-
+    try:
+        all_games = fetch_games()
+        print(f"Gathered {len(all_games)} games")
+    except requests.RequestException as e:
+        print(f"Couldn't get data from api: {e}")
 if __name__ == "__main__":
     main()
