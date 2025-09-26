7bbe858e261422550ec7267c918393e6076c7735
Mclovinggood <jthand293@gmail.com>
2025-09-26 18:35:14 -0400

    Added filter_games function to let the user search

--- main.py
+++ main.py
@@ -6,12 +6,22 @@ def fetch_games():
     response.raise_for_status()
     return response.json()
 
+def filter_games(games, search_term):
+    search_term_lower = search_term.lower()
+    return [game for game in games if search_term_lower in game['title'].lower()]
+
 def main():
-    print("FreeToGame App Jumpscare!!!")
+    print("FreeToGame app Jumpscare!!!")
+    search_term = input("Search for a game:").strip()
+    if not search_term:
+        print("Try again buddy")
+        return
     try:
         all_games = fetch_games()
-        print(f"Gathered {len(all_games)} games")
+        matching_games = filter_games(all_games, search_term)
+        print(f"Found {len(matching_games)} matching games.")
     except requests.RequestException as e:
-        print(f"Couldn't get data from api: {e}")
+        print(f"Couldn't from data from api: {e}")
+
 if __name__ == "__main__":
     main()
